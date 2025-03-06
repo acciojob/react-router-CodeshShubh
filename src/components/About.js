00ee79d1e457fbc10e98124de9e2c23e9299c433
CodeshShubh <codeshshubh@gmail.com>
2025-03-06 21:30:54 +0530

    completed

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>This is a sample React Router program.</div>
+  )
+}
+
+export default About
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import {BrowserRouter , Link, Route , Routes} from 'react-router-dom'
+import Home from './Home'
+import About from "./About";
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      
+      <BrowserRouter>
+
+        <nav>
+         <Link to={'/home'}>Home</Link>  | 
+         <Link to={'/about'}>About</Link>
+       </nav>
+      <Routes>
+        <Route path="/home" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route />
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
